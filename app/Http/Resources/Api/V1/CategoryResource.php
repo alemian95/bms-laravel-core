a9c542aafc1bcb9bde035635a72061e477429bf0
Alessandro Mian <alessandromian95@gmail.com>
2026-04-29 21:06:07 +0200

    feat(api): add CategoryResource

---
+++ app/Http/Resources/Api/V1/CategoryResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources\Api\V1;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @mixin Category
+ */
+class CategoryResource extends JsonResource
+{
+    /**
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'color' => $this->color,
+            'created_at' => $this->created_at?->toIso8601String(),
+        ];
+    }
+}
