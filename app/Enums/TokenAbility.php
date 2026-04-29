c6e01cc3a1fd07fd274558b6eb1b8802a2078759
Alessandro Mian <alessandromian95@gmail.com>
2026-04-29 20:29:31 +0200

    feat(auth): add TokenAbility enum

---
+++ app/Enums/TokenAbility.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Enums;
+
+enum TokenAbility: string
+{
+    case BookmarksRead = 'bookmarks:read';
+    case BookmarksCreate = 'bookmarks:create';
+    case BookmarksDelete = 'bookmarks:delete';
+    case CategoriesRead = 'categories:read';
+    case CategoriesWrite = 'categories:write';
+
+    /**
+     * @return array<int, string>
+     */
+    public static function values(): array
+    {
+        return array_map(fn (self $case) => $case->value, self::cases());
+    }
+}
